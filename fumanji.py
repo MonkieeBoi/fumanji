40213c58d42f01e8d70eeeb27acb6e2b0722ac99
MonkieeBoi <danny.pham26425@gmail.com>
2025-03-24 18:19:17 +1100

    feat: add main file

---
+++ fumanji.py
@@ -0,0 +1,33 @@
+from py_fumen import decode, VersionException
+from sys import argv
+
+
+EMOJI_MAP = {
+    ord('I'): '🟦',
+    ord('J'): '🟦',
+    ord('L'): '🟧',
+    ord('O'): '🟨',
+    ord('S'): '🟩',
+    ord('T'): '🟪',
+    ord('Z'): '🟥',
+    ord('_'): '⬛'
+}
+
+
+def main():
+    if len(argv) < 2:
+        print("invalid fumen")
+        return
+    fumen = argv[1]
+    try:
+        pages = decode(fumen)
+    except VersionException:
+        print("invalid fumen")
+        return
+    for i, page in enumerate(pages):
+        print(f"Page {i+1}")
+        print(page.get_field().string().translate(EMOJI_MAP))
+
+
+if __name__ == "__main__":
+    main()
